760010f678e420135e5cbec0a1c89ada3a2e5803
chivu1010 <cheesevu2003@gmail.com>
2024-09-29 22:31:03 +0700

    ex03.3

--- Ex03/app/src/main/java/com/example/ex03/MainActivity.java
+++ Ex03/app/src/main/java/com/example/ex03/MainActivity.java
@@ -53,6 +53,21 @@ public class MainActivity extends Activity {
                 edt3.setText("a * b =" +(a*b));
             }
         });
-        
+        btnchia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                int a = Integer.parseInt("0"+edt1.getText());
+                int b = Integer.parseInt("0"+edt2.getText());
+                if (b == 0)
+                {
+                    edt3.setText("B phai khac 0");
+                }
+                else
+                {
+                    edt3.setText("a / b =" +(a/b));
+                }
+            }
+        });
     }
 }
