70ce968d57086858a77c468634b077f76e20bbf2
chivu1010 <cheesevu2003@gmail.com>
2024-10-24 16:37:21 +0700

    ex10.2

--- Ex10/app/src/main/java/com/example/ex10/MySmsReceive.java
+++ Ex10/app/src/main/java/com/example/ex10/MySmsReceive.java
@@ -7,3 +7,28 @@ import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
 
+public class MySmsReceive extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        processReceive(context, intent);
+    }
+
+    private void processReceive(Context context, Intent intent) {
+        Bundle extras = intent.getExtras();
+        String message = "";
+        String body = "";
+        String address = "";
+        if (extras != null) {
+            Object[] smsEtra = (Object[]) extras.get("pdus");
+            for (int i = 0; i < smsEtra.length; i++) {
+                SmsMessage sms = SmsMessage.createFromPdu((byte[]) smsEtra[i]);
+                body = sms.getMessageBody();
+                address = sms.getOriginatingAddress();
+                message += "Co 1 tin nhan tu" + address + "\n" + body + "vua gui den";
+            }
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        }
+    }
+
+}
